2b5b9332f6276bf1fbdf4f2db43a41d2119d80b5
ThomasF85 <thomas.foeldi@gmail.com>
2023-07-24 14:45:51 +0200

    add mongoose

--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -1,5 +1,12 @@
-import { jokes } from "../../../lib/data.js";
+import dbConnect from "@/db/connect.js";
+import Joke from "@/db/models/Joke.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  // connect to database
+  await dbConnect();
+
+  // read from database
+  const jokes = await Joke.find();
+  console.log(jokes);
   response.status(200).json(jokes);
 }
